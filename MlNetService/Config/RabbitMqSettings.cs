bf092150a27df80cfdcf3f7003b2e93c5941ca81
Pedro Jesus <Pedromra.04@gmail.com>
2025-05-27 23:44:46 -0300

    feat: implement MlNetService with RabbitMQ integration

---
+++ MlNetService/Config/RabbitMqSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace MlNetService.Config
+{
+	public class RabbitMqSettings
+	{
+		public string Host { get; set; } = "";
+		public string Username { get; set; } = "";
+		public string Password { get; set; } = "";
+		public string Queue { get; set; } = "";
+	}
+
+}
---
+++ MlNetService/Infra/Messaging/Consumer/IoTMessageConsumer.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using Newtonsoft.Json.Linq;
+
+
+namespace MlNetService.Infra.Messaging.Consumer
+{
+	public class IoTMessageConsumer : IConsumer<JObject>
+	{
+		public async Task Consume(ConsumeContext<JObject> context)
+		{
+			var json = context.Message;
+			Console.WriteLine($"Recebido: {json}");
+
+			await Task.CompletedTask;
+		}
+	}
+
+}
