43440e71acba84103a1013e58d8b6e7c8f279246
Pedro Jesus <Pedromra.04@gmail.com>
2025-06-03 16:56:10 -0300

    feat(user): add new migration

---
+++ UserService/Infra/Migrations/20250603195242_seila.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class seila : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "firebase_id",
+                table: "tb_users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "firebase_id",
+                table: "tb_users");
+        }
+    }
+}
